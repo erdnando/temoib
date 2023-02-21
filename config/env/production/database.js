40b3fb6f126c479e6b5abaa2bc9073ca253fd0c8
erdnando <erdnando@gmail.com>
2023-02-21 11:46:50 -0600

    preparing heroku

---
+++ config/env/production/database.js
@@ -0,0 +1,16 @@
+// path: ./config/env/production/database.js
+
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'mysql',
+      connection: {
+        host: env('DATABASE_HOST', '162.241.60.205'),
+        port: env.int('DATABASE_PORT', 3306),
+        database: env('DATABASE_NAME', 'pymecomm_temoib'),
+        user: env('DATABASE_USERNAME', 'pymecomm_temoib'),
+        password: env('DATABASE_PASSWORD', 'Adqdisp06ERV'),
+        ssl: env.bool('DATABASE_SSL', true),
+      },
+    },
+  });
+  
